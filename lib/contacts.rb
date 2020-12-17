94431900d70e19ad44baea85e4e81c46e3d6a415
Danny Brandt <dvbrandt90@gmail.com>
2020-12-17 04:16:50 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,11 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, data| 
+    data.each do |key, value|
+      if key == favorite_ice_cream_flavors
+        key.each {|i| if i == "strawberry"}
+      end
+    end
+  end
 end
